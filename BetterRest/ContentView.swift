540d37df2f57b03603f962182574126ccec51fc6
Blergborg <pprater92@gmail.com>
2023-09-01 17:57:06 -0700

    Added ML model SleepCalculator to project files. Added function body to calcluateBedtime(), uses ML model to calculate when user should go to sleep based on input values. Added state variables for an alert that displays the result of calculateBedtime(). Added .alert() to the end of VStack.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -6,11 +6,17 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
+    // Input states
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
     @State private var coffeeAmount = 1
+    // Alert states
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         NavigationView {
@@ -34,12 +40,34 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateBedtime)
             }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
         
     
     func calculateBedtime() {
-        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is…"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        showingAlert = true
     }
 }
 
